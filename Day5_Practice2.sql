7bd7830fbde9d6cb28434f1fbb76619e972b5d74
Callme-Nhu <112468278+Callme-Nhu@users.noreply.github.com>
2023-11-14 15:53:47 +0700

    Update Day5_Practice2.sql

--- Day5_Practice2.sql
+++ Day5_Practice2.sql
@@ -28,7 +28,29 @@ FROM monthly_cards_issued
 GROUP BY card_name
 ORDER BY difference DESC
 ---Exercise 8:
+SELECT manufacturer, 
+COUNT(drug) AS drug_count,
+ABS(SUM(cogs-total_sales)) AS Total_loss
+FROM pharmacy_sales
+WHERE total_sales < cogs
+GROUP BY manufacturer
+ORDER BY Total_loss DESC
 ---Exercise 9:
+SELECT * FROM CINEMA
+WHERE ID%2=1 AND DESCRIPTION != 'boring'
+ORDER BY RATING DESC
 ---Exercise 10:
+SELECT TEACHER_ID,
+COUNT(DISTINCT SUBJECT_ID) AS CNT
+FROM TEACHER
+GROUP BY TEACHER_ID
 ---Exercise 11:
+SELECT USER_ID,
+COUNT(FOLLOWER_ID) AS FOLLOWERS_COUNT
+FROM FOLLOWERS
+GROUP BY USER_ID
+ORDER BY USER_ID ASC
 ---Exercise 12:
+SELECT CLASS FROM COURSES
+GROUP BY CLASS
+HAVING COUNT(CLASS) >=5
