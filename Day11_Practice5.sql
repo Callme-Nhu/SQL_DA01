e844c660fe8cb363e0ac8ca58cfc3270d51aa8f3
Callme-Nhu <112468278+Callme-Nhu@users.noreply.github.com>
2023-11-20 12:40:43 +0700

    Update Day11_Practice5.sql

--- Day11_Practice5.sql
+++ Day11_Practice5.sql
@@ -43,3 +43,9 @@ GROUP BY a.reports_to
 ORDER BY a.reports_to
 ---Exercise 6:
 ---Exercise 7:
+SELECT t1.page_id 
+FROM pages AS t1
+LEFT JOIN page_likes AS t2
+ON t1.page_id=t2.page_id
+WHERE t2.liked_date IS NULL
+ORDER BY t1.page_id 
