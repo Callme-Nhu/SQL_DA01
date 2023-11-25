f8fba80bd9e93198b1e716b4f56f2d51a7a17ce5
Callme-Nhu <112468278+Callme-Nhu@users.noreply.github.com>
2023-11-26 00:04:15 +0700

    Update Day13_Practice6.sql

--- Day13_Practice6.sql
+++ Day13_Practice6.sql
@@ -1,1 +1,13 @@
-
+---Exercise 4:
+SELECT t1.page_id 
+FROM pages AS t1
+LEFT JOIN page_likes AS t2
+ON t1.page_id=t2.page_id
+WHERE t2.liked_date IS NULL
+ORDER BY page_id
+---Exercise 5:
+---Exercise 6:
+---Exercise 7:
+---Exercise 8:
+---Exercise 9:
+---Exercise 10:
