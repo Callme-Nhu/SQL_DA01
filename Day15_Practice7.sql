7e4384493573584b230425a4570f26a1c18073a7
Callme-Nhu <112468278+Callme-Nhu@users.noreply.github.com>
2023-11-29 23:24:51 +0700

    Update Day15_Practice7.sql

--- Day15_Practice7.sql
+++ Day15_Practice7.sql
@@ -5,6 +5,11 @@ LAG(spend) OVER(PARTITION BY product_id) AS prev_year_spend,
 ROUND((spend-LAG(spend) OVER(PARTITION BY product_id))*100/LAG(spend) OVER(PARTITION BY product_id),2) AS yoy_rate
 FROM user_transactions;
 ---Exercise 2:
+SELECT * FROM (SELECT card_name,
+FIRST_VALUE(issued_amount) OVER(PARTITION BY card_name ORDER BY issue_year,issue_month) AS issued_amount
+FROM monthly_cards_issued) AS a
+GROUP BY card_name,issued_amount
+ORDER BY issued_amount DESC;
 ---Exercise 3:
 ---Exercise 4:
 ---Exercise 5:
