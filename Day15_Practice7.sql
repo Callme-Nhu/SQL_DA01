ba13ede098e22e998625f32fe6c7595784700f81
Callme-Nhu <112468278+Callme-Nhu@users.noreply.github.com>
2023-11-29 23:07:35 +0700

    Create Day15_Practice7.sql

---
+++ Day15_Practice7.sql
@@ -0,0 +1,13 @@
+---Exercise 1:
+SELECT EXTRACT(year FROM transaction_date) AS year ,
+product_id, spend AS curr_year_spend,
+LAG(spend) OVER(PARTITION BY product_id) AS prev_year_spend,
+ROUND((spend-LAG(spend) OVER(PARTITION BY product_id))*100/LAG(spend) OVER(PARTITION BY product_id),2) AS yoy_rate
+FROM user_transactions;
+---Exercise 2:
+---Exercise 3:
+---Exercise 4:
+---Exercise 5:
+---Exercise 6:
+---Exercise 7:
+---Exercise 8:
