a2519627a3d99aa50c394b7629bd5fe451f37499
Callme-Nhu <112468278+Callme-Nhu@users.noreply.github.com>
2023-11-29 23:35:21 +0700

    Update Day15_Practice7.sql

--- Day15_Practice7.sql
+++ Day15_Practice7.sql
@@ -11,6 +11,10 @@ FROM monthly_cards_issued) AS a
 GROUP BY card_name,issued_amount
 ORDER BY issued_amount DESC;
 ---Exercise 3:
+SELECT user_id, spend,transaction_date FROM (SELECT *,
+ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY transaction_date) AS xephang
+FROM transactions) AS a
+WHERE xephang=3
 ---Exercise 4:
 ---Exercise 5:
 ---Exercise 6:
