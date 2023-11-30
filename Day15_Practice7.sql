de45bbfde6848f4b7a3d0970bf1eb2a1326f1ddf
Callme-Nhu <112468278+Callme-Nhu@users.noreply.github.com>
2023-11-30 12:32:52 +0700

    Update Day15_Practice7.sql

--- Day15_Practice7.sql
+++ Day15_Practice7.sql
@@ -17,6 +17,10 @@ FROM transactions) AS a
 WHERE xephang=3
 ---Exercise 4:
 ---Exercise 5:
+SELECT user_id, tweet_date,
+ROUND(AVG(tweet_count) OVER (PARTITION BY user_id ORDER BY tweet_date 
+ROWS BETWEEN 2 PRECEDING AND CURRENT ROW),2) AS rolling_avg_3d
+FROM tweets
 ---Exercise 6:
 ---Exercise 7:
 ---Exercise 8:
